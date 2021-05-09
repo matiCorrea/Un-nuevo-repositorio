0ed19bc0c11f33e60c9e48c899f037a66c7849fe
Matias Correa <matiascorrea@live.com>
2021-05-09 18:18:30 -0300

    repositorio nuevo

---
+++ index.ts
@@ -0,0 +1,15 @@
+class Banda {
+  members: string[];
+  albums: any[];
+  constructor(members: string[], albums: any[]) {
+    this.members = members;
+    this.albums = albums;
+  }
+}
+
+//function main() {
+//  const unaBanda = new Banda(["mati"], ["hola"]);
+//  console.log(unaBanda.albums);
+//  console.log(unaBanda.members);
+//}
+//main();
